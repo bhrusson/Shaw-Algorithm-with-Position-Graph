7c07927d94a8fe555d73f0261209b13f1b995837
udel-bhrusson <126599227+udel-bhrusson@users.noreply.github.com>
2025-07-28 14:21:58 -0400

    Updated many functions and some data structures for the position graph

--- bqskit/position/graph.py
+++ bqskit/position/graph.py
@@ -1,17 +1,8 @@
 import numpy as np
 import rustworkx as rx
 from enum import Enum
-from typing import Tuple, List, Sequence, Mapping
-from dataclass import dataclass
-
-#   Information to be represented:
-#   number of qudits - 
-#   radices of quidits (list of integers where the index of an integer represents the basis or radix of a qudit.  radices[4] == 3 implies qudit 4 is a qutrit
-#   How the qudits are connected - What types of different connections are there? i.e. swap, move, - defines what types of operations could be possible
-#   abilities of each qubits, (be measured, executed, storage,)
-#   adjacenecy graph, adjacency 
-
-#   rustworkx
+from typing import Tuple, List, Sequence, Mapping, Dict, Callable
+from dataclasses import dataclass
 
 class PositionCapability(Enum):
     NONE = 0
@@ -20,87 +11,110 @@ class PositionCapability(Enum):
     STARTING = 0b100
 
 @dataclass
-class VertexLabel:
+class PositionLabel:
     capability: int
+    weights: Dict[int, float]
+
 
     def has_capability(self, capability: PositionCapability) -> bool:
         return self.capability & capability != 0
-
+    
+    def get_weight(self, capability: PositionCapability) -> float:
+        if not self.has_capability(capability):
+            raise ValueError(f"Capability {capability.name} not present in this edge")
+        return self.weights.get(capability, float('inf')) 
 
 # TODO (BRENT): Read dataclass documentation, update the other labels accordingly
 # Combine EdgeCapabilities into one (Execute doesn't need to be separate from moovement (in implmentation))
+#
+# Brent - I have attempted to do this.
 
-class EdgeMovementLabel(Enum):
+class EdgeCapability(Enum):
     NONE = 0
-    MOVE = 1
-    SWAP = 2
-    MOVE_SWAP = 3
+    MOVE = 0b001
+    SWAP = 0b010
+    EXECUTE = 0b100
 
-class EdgeExecutionLabel(Enum):
-    NONE = 0
-    EXECUTE = 1
+@dataclass
+class EdgeLabel:
+    capability: int
+    weights: Dict[int, float]
 
-EdgeLabel = Tuple[EdgeMovementLabel, EdgeExecutionLabel]
-
+    def has_capability(self, capability: EdgeCapability) -> bool:
+          return self.capability & capability != 0
+     
+    def get_weight(self, capability: EdgeCapability) -> float:
+        if not self.has_capability(capability):
+            raise ValueError(f"Capability {capability.name} not present in this edge")
+        return self.weights.get(capability, float('inf'))
 
 class PositionGraph:
     def __init__(
             self,
-            radices: Sequence[int],
-            node_labels: Sequence[VertexLabel], #Length of this is the number of Positions availble for qudits
-            connection_labels: Mapping[Tuple[int, int], EdgeLabel] #Key value type of information, use as a dictionary
+            radices: Sequence[int], #Length of this is the number of qudits
+            pos_labels: Sequence[PositionLabel], #Length of this is the number of Positions availble for qudits
+            edge_labels: Mapping[Tuple[int, int], EdgeLabel] #Key value type of information, use as a dictionary
         ) -> None:
-        self.radices = list(radices)
-        self.node_labels = list(node_labels)
-        self.connection_labels = connection_labels
+        self._radices = list(radices)
+        self._pos_labels = list(pos_labels)
+        self._edge_labels = edge_labels
 
-        self.graph = rx.PyDiGraph()
-        self.graph.add_nodes_from(node_labels)
-        self.graph.add_edges_from(list(connection_labels.items()))
+        self._graph = rx.PyDiGraph()
+        self._graph.add_nodes_from(pos_labels)
+        self._graph.add_edges_from(list(edge_labels.items()))
 
     def check_node_index(self, index: int) -> None:
-        if (index < 0 or index >= len(self.node_labels)):
-            raise ValueError(f"Invalid index: {index} \nValid range: 0 to {len(self.node_labels)-1}")
+        if (index < 0 or index >= len(self.pos_labels)):
+            raise ValueError(f"Invalid index: {index} \nValid range: 0 to {len(self.pos_labels)-1}")
 
     @property
     def get_num_qudits(self) -> int:
-        return len(self.radices)
+        return len(self._radices)
+    
+    @property
+    def graph(self) -> rx.PyDiGraph:
+        return self._graph
    
     @property
     def radices(self) -> list[int]:
-         return self.radices
+         return self._radices
 
     @property
-    def position_labels(self) -> list[VertexLabel]:
-        return self.node_labels
+    def position_labels(self) -> list[PositionLabel]:
+        return self._pos_labels
+    
+    @property
+    def edge_labels(self) ->  Dict[Tuple[int, int], EdgeLabel]:
+        return self._edge_labels
 
-    def position_label(self, node_index: int) -> VertexLabel:
-        self.check_node_index(node_index)
-        return self.node_labels[node_index]
+    def position_label(self, pos_index: int) -> PositionLabel:
+        self.check_node_index(pos_index)
+        return self.position_labels[pos_index]
 
     # TODO: Brent Calling code should use the position vocabulary not the node vocab...
-    def position_has_capability(self, node_index: int, capability: PositionCapability) -> bool:
-        pass
+    def position_has_capability(self, pos_index: int, capability: PositionCapability) -> bool:
+        self.check_node_index(pos_index)
+        return self.position_labels[pos_index].has_capability(capability)
+
 
     # todo like above
     def get_all_edge_labels(self) -> List[EdgeLabel]:
-            return [label for _, _, label in self.connection_labels]
+            return [label for _, _, label in self.edge_labels]
 
     # todo like above
     def get_edge_label(self, edge_index1: int, edge_index2: int) -> EdgeLabel:
-            # Find the edge in connection_labels
-            for u, v, label in self.connection_labels:
-                if u == edge_index1 and v == edge_index2:
-                    return label
-            raise KeyError(f"Edge ({edge_index1} -> {edge_index2}) not found.")
+            try:
+                 return self.edge_labels[(edge_index1, edge_index2)]
+            except KeyError:
+                 raise KeyError(f"Edge ({edge_index1} -> {edge_index2}) not found.")
 
     # get nodes with capabilities
-    def get_nodes_with_label(self, label: VertexLabel) -> List[int]:
-            return [i for i, node_label in enumerate(self.node_labels) if node_label == label]
+    def get_positions_with_label(self, label: PositionLabel) -> List[int]:
+            return [i for i, node_label in enumerate(self.position_labels) if node_label == label]
 
     # this is the projected graph thingy
     def get_edges_with_label(self, label: EdgeLabel) -> List[Tuple[int, int]]:
-            return [(u, v) for u, v, edge_label in self.connection_labels if edge_label == label]
+            return [(u, v) for u, v, edge_label in self.edge_labels if edge_label == label]
    
     # position
     def get_neighbors(self, node_index: int) -> List[int]:
@@ -118,54 +132,74 @@ class PositionGraph:
     def get_ancestors(self, node_index: int) -> List[int]:
          return rx.ancestors(self.graph,node_index)
     
-    def get_descendents(self, node_index: int) -> List[int]:
-         return self.graph.predecessors(node_index)
+    def get_descendants(self, node_index: int) -> List[int]:
+         return  rx.descendants(self.graph, node_index)
     
-    def find_successors_by_edge(self, node_index: int, edge_filter_function: function) -> List[int]:
-         return self.graph.find_successors_by_edge(node_index,edge_filter_function)
+    def find_successors_by_edge(self, node_index: int, edge_filter: Callable) -> List[int]:
+         return self.graph.find_successors_by_edge(node_index,edge_filter)
     
-    def can_execute_node(self, node_index: int) -> bool:
-         return (self.get_node_label(node_index).value == 1 or self.get_node_label(node_index).value == 3)
+    def can_execute_position(self, node_index: int) -> bool:
+         return self.position_label(node_index).has_capability(PositionCapability.EXECUTE)
     
-    def can_measure_node(self, node_index: int) -> bool:
-         return (self.get_node_label(node_index).value == 2 or self.get_node_label(node_index).value == 3)
+    def can_measure_position(self, node_index: int) -> bool:
+         return self.position_label(node_index).has_capability(PositionCapability.MEASURE)
+    
+    def can_be_starting_position(self, node_index: int) -> bool:
+         return self.position_label(node_index).has_capability(PositionCapability.STARTING)
     
     def can_execute_edge(self, edge_index1: int, edge_index2: int) -> bool:
-        return self.get_edge_label(edge_index1,edge_index2)[1].value == 1
+        return self.get_edge_label(edge_index1,edge_index2).has_capability(EdgeCapability.EXECUTE)
        
     def can_move_edge(self, edge_index1: int, edge_index2: int) -> bool:
-        return (self.get_edge_label(edge_index1,edge_index2)[0].value == 2 or self.get_edge_label(edge_index1,edge_index2)[0].value == 3)
+        return self.get_edge_label(edge_index1,edge_index2).has_capability(EdgeCapability.MOVE)
     
-    #not finished
-    def can_move(self, src: int, dst: int) -> bool:
-        self.check_node_index(src)
-        self.check_node_index(dst)
-        label = self.connection_labels.get((src, dst))
-        return label is not None and label[0] in [EdgeMovementLabel.MOVE, EdgeMovementLabel.MOVE_SWAP]
-
-    #not finished
-    def can_swap(self, src: int, dst: int) -> bool:
-        self.check_node_index(src)
-        self.check_node_index(dst)
-        label = self.connection_labels.get((src, dst))
-        return label is not None and label[0] in [EdgeMovementLabel.SWAP, EdgeMovementLabel.MOVE_SWAP]
+    def can_swap_edge(self, edge_index1: int, edge_index2: int) -> bool:
+        return self.get_edge_label(edge_index1,edge_index2).has_capability(EdgeCapability.SWAP)
     
-    #not finished
-    def path_to(self, src: int, dst: int) -> List[int]:
-        self.check_node_index(src)
-        self.check_node_index(dst)
-        path = rx.digraph_dijkstra_shortest_paths(self.graph, src)[0].get(dst)
-        if path is None:
-            raise ValueError(f"No path from {src} to {dst}")
-        return path
 
-    def get_projected_graph(self, edge_capability: TODO) -> list[Edge]:
-        """TODO: BRENT"""
-        pass
+    def get_subgraph_by_position_capibility(self, position_capability: PositionCapability) -> rx.PyDiGraph:
+        valid_nodes = [i for i, label in enumerate(self.position_labels) if label.has_capability(position_capability)]
+        return self._graph.subgraph(valid_nodes)
+        
+ 
+    def get_projected_graph(self, edge_capability: EdgeCapability, weight_filter: Callable[[EdgeLabel],bool] = None) -> rx.PyDiGraph:
+        # Filter edges based on capability and optional weight filter
+        edges_to_include = []
+        for (u, v), label in self._edge_labels.items():
+            if label.has_capability(edge_capability):
+                if weight_filter is None or weight_filter(label):
+                    edges_to_include.append((u, v))
+        
+        # Get all nodes connected by these edges (to avoid isolated nodes)
+        nodes_to_include = set()
+        for u, v in edges_to_include:
+            nodes_to_include.add(u)
+            nodes_to_include.add(v)
+        
+        # Use rustworkx's subgraph method: it takes a list of node indices
+        subgraph = self._graph.subgraph(list(nodes_to_include))
+        
+        # Now we need to filter edges within subgraph to only those edges we want,
+        # because subgraph keeps *all* edges between those nodes.
+        # So let's remove edges that don't match capability and weight_filter:
+        edges_to_remove = []
+        for edge in subgraph.edge_list():
+            u, v, _ = edge
+            label = self._edge_labels.get((u, v))
+            if label is None or not label.has_capability(edge_capability) or (weight_filter and not weight_filter(label)):
+                edges_to_remove.append((u, v))
+        
+        for u, v in edges_to_remove:
+            subgraph.remove_edge(subgraph.find_edge(u, v))
+        
+        return subgraph
     
     def get_valid_starting_position(self) -> list[int]:
-        """TODO: not all postions are valid starting positions for physical qubits. this should return the valid ones."""
-        pass
+        return[
+            index
+            for index, label in enumerate(self.position_labels)
+            if label.has_capability(PositionCapability.STARTING)
+        ]
 
     
     #Can functions, Can move from 1 position to another, How to move from 1 pos to another, 
